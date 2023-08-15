b5a696eb075e2620e236eea88fff7d537b602ccb
Sergey Tsybulkin <sereg1ns@yandex.ru>
2023-08-15 23:05:22 +0300

    F3

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -3,23 +3,24 @@ export const BASE_URL = "https://api.sereg1ns.nomoreparties.co";
 
 
 export const register = (email, password) => {
-  return fetch(`${BASE_URL}/signup`, {
-    method: "POST",
+  return fetch(`${BASE_URL}/sign-up`, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ email, password })
   }).then((res) => getResponseData(res));
 };
 
 export const authorize = (email, password) => {
-  return fetch(`${BASE_URL}/signin`, {
-    method: "POST",
+  return fetch(`${BASE_URL}/sign-in`, {
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ email, password })
   }).then((res) => getResponseData(res));
 };
 
